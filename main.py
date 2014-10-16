91e68c18bed06dc3328868191fb35e7d586837d2
Prajakta <prajakta.n.mane@gmail.com>
2014-10-16 14:40:03 -0600

    Split and Join: Does not work for indentation.

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import fileinput
 
-for line in fileinput.FileInput('foo.txt'):
+for line in fileinput.FileInput('change.txt'):
   
       
     # looks for an empty line or comment
     if not line or line.startswith('#'):
         continue
         
-    if "Erica" in line:
+    if "CTA_SIZE" in line:
 
     # loops through the lines in the write file
-        for linef2 in fileinput.FileInput('writeFile.txt', inplace=1):
+        for linef2 in fileinput.FileInput('constants.h', inplace=1):
     
             linef2= linef2.strip()
-            if "Gaga" in linef2:
+            if "CTA_SIZE" in linef2:
                 line = line.strip()
-                s = linef2.replace('Gaga',line)
+                s = linef2.split("CTA_SIZE", 1)[0]
+                str = " "
+                seq = [s, line]
+                s = str.join(seq)
                 print s
             else:
                 print linef2
