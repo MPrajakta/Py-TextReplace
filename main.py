00ff34461252143cf120e5eb866ac8867eef96e3
Prajakta <prajakta.n.mane@gmail.com>
2014-10-16 11:33:33 -0600

    Write into an empty text file

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import fileinput
 
+# open a file
 f1 = open('foo.txt')
-#f2 = open('foo.txt.tmp', 'w')
+
+# open another file to write
+f2 = open('writeFile.txt', 'w')
 
 for line in f1:
-    print "This is a line"
-    linename = line.strip()
-    if not linename or linename.startswith('#'):
+    #print "This is a line"
+    #linename = line.strip()
+    
+    # looks for an empty line or comment
+    if not line or line.startswith('#'):
         continue
-#    f1.write(line.replace('This', 'That'))    
-    #print linename 
+    #f1.write(line.replace('This', 'That'))    
+    
+    # write text in an empty file
+    f2.write(line)
 
 #for line in fileinput.input('foo.txt'):
 #    linename = line.strip()
