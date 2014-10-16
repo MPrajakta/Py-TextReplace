e7729ad88ce1ea290efe3ddd4ece4153eb17f5cc
Prajakta <prajakta.n.mane@gmail.com>
2014-10-16 13:49:57 -0600

    Search for text in one file and replace a particular text in another file

--- main.py
+++ main.py
@@ -1,46 +1,22 @@
 import fileinput
 
-# open a file
-# f1 = open('foo.txt')
-
-# open another file to write
-f2 = open('writeFile.txt', 'r')
-
-#f3 = open('writeFile.txt', 'w')
-
-#for line in f1:
+for line in fileinput.FileInput('foo.txt'):
   
       
     # looks for an empty line or comment
-    #if not line or line.startswith('#'):
-        #continue
+    if not line or line.startswith('#'):
+        continue
+        
+    if "Erica" in line:
 
     # loops through the lines in the write file
-for linef2 in fileinput.FileInput('writeFile.txt', inplace=1):
-    #print linef2
-    linef2= linef2.strip()
-    if "This" in linef2:
-        #print linef2
-        s = linef2.replace('This','Yeah')
-        
-        print s
-    else:
-        print (linef2)
-        #f3 = open('writeFile.txt', 'w')
-        
-        #y = f3.write(s)
-        #f3.close()
-        #print y
-
-#f3.close
-
-
-    # replace 'This' with 'That' in the write file 
-    #f2.write(line.replace('This', 'That'))    
+        for linef2 in fileinput.FileInput('writeFile.txt', inplace=1):
     
-    # write text in an empty file
-    #f2.write(line)
-
-#for line in fileinput.input('foo.txt'):
-#    linename = line.strip()
-#    print linename
+            linef2= linef2.strip()
+            if "Gaga" in linef2:
+                line = line.strip()
+                s = linef2.replace('Gaga',line)
+                print s
+            else:
+                print linef2
+           
