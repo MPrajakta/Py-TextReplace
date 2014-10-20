931bc259917d9717f48c6d9c690e1dab8b85598f
Prajakta <prajakta.n.mane@gmail.com>
2014-10-19 22:05:35 -0600

    Commented print to STDOUT

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ for line in fileinput.FileInput('change.txt'):
             else:
                 print linef2.rstrip()
     elif "WARP_SIZE" in line:
-        print line
+        #print line
         for line2 in fileinput.FileInput('constants.h', inplace =1):
             if "WARP_SIZE" in line2:
                 line2 = line2.strip()
@@ -37,7 +37,7 @@ for line in fileinput.FileInput('change.txt'):
             else:
                 print line2.rstrip()
     elif "__items_per_thread" in line:
-        print line
+        #print line
         for line2 in fileinput.FileInput('constants.h', inplace =1):
             if "__items_per_thread" in line2:
                 line2 = line2.strip()
@@ -50,7 +50,7 @@ for line in fileinput.FileInput('change.txt'):
             else:
                 print line2.rstrip()
     elif "__num_ctas" in line:
-        print line
+        #print line
         for line2 in fileinput.FileInput('constants.h', inplace =1):
             if "__num_ctas" in line2:
                 line2 = line2.strip()
