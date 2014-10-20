6a986a03c198303be9687aae70fb83c8f8be0313
Prajakta <prajakta.n.mane@gmail.com>
2014-10-19 21:24:11 -0600

    Elif with second condition

--- main.py
+++ main.py
@@ -18,10 +18,21 @@ for line in fileinput.FileInput('change.txt'):
                 line = line.strip()
                 s = linef2.split("CTA_SIZE", 1)[0]
                 str = ""
-                seq = ["",s, line,";"]
+                seq = ["\t",s, line,";"]
                 s = str.join(seq)
                 print s.rstrip()
             else:
                 print linef2.rstrip()
     elif "NUM_SIZE" in line:
         print line
+        for line2 in fileinput.FileInput('constants.h', inplace =1):
+            if "NUM_SIZE" in line2:
+                line2 = line2.strip()
+                line =line.strip()
+                s = line2.split("NUM_SIZE", 1)[0]
+                str =""
+                seq = ["\t", s, line, ";"]
+                s=str.join(seq)
+                print s.rstrip()
+            else:
+                print line2.rstrip()
