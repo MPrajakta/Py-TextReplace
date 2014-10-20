2107b6bf99df60f9d9cb539c7148f0f6a7e67165
Prajakta <prajakta.n.mane@gmail.com>
2014-10-19 22:01:54 -0600

    Conditions for all 4 cases

--- main.py
+++ main.py
@@ -23,16 +23,42 @@ for line in fileinput.FileInput('change.txt'):
                 print s.rstrip()
             else:
                 print linef2.rstrip()
-    elif "NUM_SIZE" in line:
+    elif "WARP_SIZE" in line:
         print line
         for line2 in fileinput.FileInput('constants.h', inplace =1):
-            if "NUM_SIZE" in line2:
+            if "WARP_SIZE" in line2:
                 line2 = line2.strip()
                 line =line.strip()
-                s = line2.split("NUM_SIZE", 1)[0]
+                s = line2.split("WARP_SIZE", 1)[0]
                 str =""
                 seq = ["\t", s, line, ";"]
                 s=str.join(seq)
+                print s.rstrip()
+            else:
+                print line2.rstrip()
+    elif "__items_per_thread" in line:
+        print line
+        for line2 in fileinput.FileInput('constants.h', inplace =1):
+            if "__items_per_thread" in line2:
+                line2 = line2.strip()
+                line =line.strip()
+                s =line2.split("__items_per_thread", 1)[0]
+                str = ""
+                seq = ["\t", s, line, ";"]
+                s=str.join(seq)
+                print s.rstrip()
+            else:
+                print line2.rstrip()
+    elif "__num_ctas" in line:
+        print line
+        for line2 in fileinput.FileInput('constants.h', inplace =1):
+            if "__num_ctas" in line2:
+                line2 = line2.strip()
+                line = line.strip()
+                s =line2.split("__num_ctas", 1)[0]
+                str =""
+                seq =["\t", s, line, ";"]
+                s= str.join(seq)
                 print s.rstrip()
             else:
                 print line2.rstrip()
